94b0b251766ca35802f13c00d5a4760766a7e7f7
Matthew <64306058+matthew-lc-zheng@users.noreply.github.com>
2021-06-06 15:21:50 +0800

    Update and rename test.py to performance_test.py

--- numpy test/performance_test.py
+++ numpy test/performance_test.py
@@ -1,17 +1,15 @@
 from timeit import default_timer as timer
 import numpy as np
 
-d=100000000
+d=10000
 n=20
 t=[]
 
 # consttruction
 for i in range(n):
     t1=timer()        
-    m1=np.ones(d)
+    m1=np.ones((d,d))
     t2=timer()
     t.append(1000*(t2-t1))
-
-# print(t)
 print('construction with initialization: %f ms' % np.mean(t))
 
